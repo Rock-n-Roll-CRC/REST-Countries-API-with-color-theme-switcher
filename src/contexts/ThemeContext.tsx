7be6f6a62be4c5be7531e8292d517e51583ee7aa
Danil Dikhtyar <danil.dikhtyar@gmail.com>
2024-12-12 00:22:34 +0700

    🚧 Implement ThemeContext and useTheme custom hook

---
+++ src/contexts/ThemeContext.tsx
@@ -0,0 +1,47 @@
+import { type ReactNode, createContext, useState, useEffect } from "react";
+
+const initialValue = {
+  theme: "light",
+  nextTheme: "dark",
+  icon: "moon-outline",
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  toggleTheme: () => {},
+};
+
+const ThemeContext = createContext(initialValue);
+
+const ThemeProvider = ({ children }: { children: ReactNode }) => {
+  const [theme, setTheme] = useState(initialValue.theme);
+
+  const nextTheme = theme === "light" ? "dark" : "light";
+  const icon = theme === "light" ? "moon-outline" : "sunny-outline";
+
+  function toggleTheme() {
+    setTheme(nextTheme);
+  }
+
+  useEffect(() => {
+    const storagedTheme = localStorage.getItem("theme");
+
+    if (storagedTheme) setTheme(storagedTheme);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  return (
+    <ThemeContext.Provider
+      value={{
+        theme,
+        nextTheme,
+        icon,
+        toggleTheme,
+      }}
+    >
+      <div className={`theme--${theme}`}>{children}</div>
+    </ThemeContext.Provider>
+  );
+};
+
+export { ThemeContext, ThemeProvider };
