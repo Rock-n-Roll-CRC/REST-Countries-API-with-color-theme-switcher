60b795a2fd0b07e51744e90c527803c0e8764584
Danil Dikhtyar <danil.dikhtyar@gmail.com>
2024-12-15 14:12:17 +0700

    🚧 Implement SearchInput component

---
+++ src/components/SearchInput/SearchInput.tsx
@@ -0,0 +1,40 @@
+import iconsURL from "../../assets/icons.svg?no-inline";
+
+import styles from "./SearchInput.module.scss";
+
+const SearchInput = ({
+  name,
+  id,
+  value,
+  onChange,
+  placeholder,
+}: {
+  name: string;
+  id: string;
+  value: string;
+  onChange: (newValue: string) => void;
+  placeholder?: string;
+}) => {
+  return (
+    <div className={styles["search-input"]}>
+      <svg className={styles["search-input__icon"]}>
+        <use href={new URL(`${iconsURL}#search-sharp`, import.meta.url).href} />
+      </svg>
+
+      <input
+        type="search"
+        name={name}
+        id={id}
+        value={value}
+        onChange={(event) => {
+          onChange(event.target.value);
+        }}
+        placeholder={placeholder}
+        autoComplete="off"
+        className={styles["search-input__input"]}
+      />
+    </div>
+  );
+};
+
+export default SearchInput;
