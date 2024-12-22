3e4b745ebf2252cfc7d18b4d6353bf0a10a7c977
Danil Dikhtyar <danil.dikhtyar@gmail.com>
2024-12-22 10:20:53 +0700

    💄 Add CountryCardList to the UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
+import { lazy, Suspense } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import { ThemeProvider } from "./contexts/ThemeContext";
 
-import Homepage from "./pages/Homepage";
+import LoadingSpinnerFullPage from "./components/LoadingSpinnerFullPage/LoadingSpinnerFullPage";
+import Message404 from "./components/Message404/Message404";
 
 import "./styles/main.scss";
+
+const Homepage = lazy(() => import("./pages/Homepage"));
 
 const App = () => {
   return (
     <ThemeProvider>
       <BrowserRouter>
-        {/* TODO: Implement code splitting */}
-        <Routes>
-          <Route index element={<Homepage />} />
-          {/* TODO: Add a route for an unknown page */}
-        </Routes>
+        <Suspense fallback={<LoadingSpinnerFullPage />}>
+          <Routes>
+            <Route index element={<Homepage />} />
+            <Route path="*" element={<Message404 />} />
+          </Routes>
+        </Suspense>
       </BrowserRouter>
     </ThemeProvider>
   );
--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,11 +1,78 @@
+// TODO: loading spinners + infinite scroll + responsive
+
+import { useState } from "react";
+import { useSearchParams } from "react-router-dom";
+
 import Header from "../components/Header/Header";
 import ThemeSwitcher from "../components/ThemeSwitcher/ThemeSwitcher";
+import Main from "../components/Main/Main";
+import CountryCardsContainer from "../components/CountryCardsContainer/CountryCardsContainer";
+import CountryCardSearchOptions from "../components/CountryCardSearchOptions/CountryCardSearchOptions";
+import SearchInput from "../components/SearchInput/SearchInput";
+import Select from "../components/Select/Select";
+import CountryCardList from "../components/CountryCardList/CountryCardList";
 
 const Homepage = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const [searchedCountryName, setSearchedCountryName] = useState(
+    searchParams.get("countryName") ?? "",
+  );
+  const [searchedCountryRegion, setSearchedCountryRegion] = useState(
+    searchParams.get("countryRegion") ?? "",
+  );
+
+  function handleChangeSearchedCountryName(searchedCountryName: string) {
+    setSearchedCountryName(searchedCountryName);
+    searchParams.set("countryName", searchedCountryName);
+    setSearchParams(searchParams);
+  }
+
+  function handleChangeSearchedCountryRegion(searchedCountryRegion: string) {
+    setSearchedCountryRegion(searchedCountryRegion);
+    searchParams.set("countryRegion", searchedCountryRegion);
+    setSearchParams(searchParams);
+  }
+
   return (
-    <Header>
-      <ThemeSwitcher />
-    </Header>
+    <>
+      <Header>
+        <ThemeSwitcher />
+      </Header>
+
+      <Main>
+        <CountryCardsContainer>
+          <CountryCardSearchOptions>
+            <SearchInput
+              name="country"
+              id="country"
+              value={searchedCountryName}
+              onChange={handleChangeSearchedCountryName}
+              placeholder="Search for a country..."
+            />
+
+            <Select
+              value={searchedCountryRegion}
+              onChange={handleChangeSearchedCountryRegion}
+              placeholder="Filter by Region"
+            >
+              {[
+                { label: "Africa", value: "africa" },
+                { label: "America", value: "americas" },
+                { label: "Asia", value: "asia" },
+                { label: "Europe", value: "europe" },
+                { label: "Oceania", value: "oceania" },
+              ]}
+            </Select>
+          </CountryCardSearchOptions>
+
+          <CountryCardList
+            searchedCountryName={searchedCountryName}
+            searchedCountryRegion={searchedCountryRegion}
+          />
+        </CountryCardsContainer>
+      </Main>
+    </>
   );
 };
 
