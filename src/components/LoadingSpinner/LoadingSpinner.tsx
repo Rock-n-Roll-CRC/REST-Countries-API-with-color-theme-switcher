d7ad2a4eb291f3c33cd1315f6db7d65414b06d0e
Danil Dikhtyar <danil.dikhtyar@gmail.com>
2024-12-23 23:28:44 +0700

    ✨ Add position prop support for LoadingSpinner

--- src/components/LoadingSpinner/LoadingSpinner.tsx
+++ src/components/LoadingSpinner/LoadingSpinner.tsx
@@ -2,10 +2,16 @@ import iconsURL from "../../assets/icons.svg?no-inline";
 
 import styles from "./LoadingSpinner.module.scss";
 
-const LoadingSpinner = ({ size }: { size?: "fullpage" }) => {
+const LoadingSpinner = ({
+  size,
+  position,
+}: {
+  size?: "fullpage";
+  position?: "centered";
+}) => {
   return (
     <svg
-      className={`${styles["loading-spinner"] ?? ""} ${(size && styles[`loading-spinner--${size}`]) ?? ""}`}
+      className={`${styles["loading-spinner"] ?? ""} ${(size && styles[`loading-spinner--${size}`]) ?? ""} ${(position && styles[`loading-spinner--${position}`]) ?? ""}`}
     >
       <use href={new URL(`${iconsURL}#3-dots-bounce`, import.meta.url).href} />
     </svg>
